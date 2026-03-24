26d0138fb0c4605f4fb9d2d5bc6f0c939db482b2
Anne Truong <anne.truongxx@gmail.com>
2026-03-24 18:11:24 +1100

    Setting up scene. Added toon shader.

---
+++ prototype-2/scripts/camera.gd
@@ -0,0 +1,28 @@
+extends Node3D
+class_name CameraController
+
+var playerController : PlayerController
+var inputRotation : Vector3
+var mouseInput : Vector2
+var mouseSensitivity : float = 0.005
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	playerController = get_parent()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		mouseInput.x += -event.screen_relative.x * mouseSensitivity
+		mouseInput.y += -event.screen_relative.y * mouseSensitivity
+
+func _process(delta: float) -> void:
+	inputRotation.x = clampf(inputRotation.x + mouseInput.y, deg_to_rad(-90), deg_to_rad(85))
+	inputRotation.y += mouseInput.x
+	
+	# rotate camera controller (up/down)
+	transform.basis = Basis.from_euler(Vector3(inputRotation.x, 0.0, 0.0))
+	
+	# rotate player (left/right)
+	playerController.global_transform.basis = Basis.from_euler(Vector3(0.0, inputRotation.y, 0.0))
+	
+	mouseInput = Vector2.ZERO
